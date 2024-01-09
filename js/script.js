f06486db99cad32f4a7bee49290deb7cec982674
c5dingb5y <jiochoi7@gmail.com>
2024-01-09 11:22:18 +0900

    Testing api

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
+api = "https://api.novelai.net/";
+
 function apiCall() {
-	$.ajaxPrefilter('json', function(options, orig, jqXHR) {
-		return 'jsonp';
-	});
-	$.ajax({
-			url: "api.novelai.net",
-			crossDomain: true,
-			dataType: "json",
-			method: "GET",
-			data: {},
-			headers: {},
-			success: function(result, textStatus, jqXHR ) {					
-			}
-	});
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST', api, true);
+	
+	xhr.onload = function() {
+		if (this.status == 200) {
+			console.log(this.responseText);
+		}
+	}
+
+	const data = {
+		"prompt": "The quick brown fox jumps over the lazy dog.",
+		"length": 50,
+		"num_samples": 1,
+		"temperature": 0.7,
+		"top_p": 1,
+		"frequency_penalty": 0,
+		"presence_penalty": 0,
+		"stop_token": "EOS"
+	};
+	
+	xhr.send(data);
 }
