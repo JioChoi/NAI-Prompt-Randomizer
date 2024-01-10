e2b354d401cedf6bb71e291858cce0a32d82582c
c5dingb5y <jiochoi7@gmail.com>
2024-01-10 12:24:46 +0900

    Change bass path

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.use('/js', express.static(__dirname + '/js'));
 
-app.use('/api', createProxyMiddleware({ target: 'http://api.novelai.net', changeOrigin: true }));
+app.use('/api', createProxyMiddleware({ target: 'http://api.novelai.net/', changeOrigin: true }));
 
 app.get('/', function(req, res, next) {
 	res.sendFile(__dirname + '/index.html');
