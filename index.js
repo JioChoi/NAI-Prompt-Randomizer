497ada2263d2a6f449ff03337a2ea0dee1e4f317
c5dingb5y <jiochoi7@gmail.com>
2024-01-10 12:42:26 +0900

    Change Origin

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.use('/js', express.static(__dirname + '/js'));
 
-app.use('/api', createProxyMiddleware({ target: 'http://api.novelai.net/', changeOrigin: false, pathRewrite: { '/api': '' } }));
+app.use('/api', createProxyMiddleware({ target: 'http://api.novelai.net/', changeOrigin: true, pathRewrite: { '/api': '' } }));
 
 app.get('/', function(req, res, next) {
 	res.sendFile(__dirname + '/index.html');
