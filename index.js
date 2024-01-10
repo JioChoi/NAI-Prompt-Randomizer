5d53965adb9a5eec56950141c7c83eace36d3779
c5dingb5y <jiochoi7@gmail.com>
2024-01-10 16:06:09 +0900

    Add many things

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ var app = express();
 
 app.use(cors());
 
+app.use(function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	next();
+});
+
 app.use('/api', createProxyMiddleware({
 	target: 'https://api.novelai.net',
 	changeOrigin: true,
@@ -16,6 +24,8 @@ app.use('/api', createProxyMiddleware({
 }));
 
 app.use('/js', express.static(__dirname + '/js'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
 app.get('/', function(req, res, next) {
 	res.sendFile(__dirname + '/index.html');
 });
--- js/script.js
+++ js/script.js
@@ -1,17 +1,47 @@
 let api = '/api';
+let key = '';
 
-async function apiCall() {
-	// using ajax
-	let url = api;
+async function argon_hash(email, password, size, domain) {
+    var pre_salt = password.slice(0, 6) + email + domain;
+    var salt = blake2b.blake2bHex(pre_salt);
+    
+    var raw = await argon2.hash({
+        pass: password,
+        salt: salt,
+        time: 2,
+        mem: Math.floor(2000000 / 1024),
+        hashLen: size,
+        type: argon2.argon2id
+    });
+
+	var b64 = Buffer.Buffer.from(raw.hash).toString('base64');
+
+	return b64;
+}
+
+async function connect(id, pw) {
+	let key = await getAccessKey(id, pw);
+	let url = api + "/user/login";
+
+	console.log(key);
+
 	$.ajax({
 		url: url,
-		type: 'GET',
-		dataType: 'text',
+		type: 'POST',
+		dataType: 'json',
+		data: {
+			"key": key
+		},
 		success: function(data) {
 			console.log(data);
 		},
-		error: function(xhr, status, error) {
-			console.log(xhr.responseText);
+		error: function(data) {
+			console.log(data);
 		}
 	});
+}
+
+async function getAccessKey(id, pw) {
+	let key = await argon_hash(id, pw, 64, "novelai_data_access_key");
+	return key.substring(0, 64);
 }
