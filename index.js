fd7b3b708fe47d412bc34e25ad767d48d2e33b09
c5dingb5y <jiochoi7@gmail.com>
2024-01-10 13:00:12 +0900

    Add responseInterceptor

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ var app = express();
 
 app.use('/js', express.static(__dirname + '/js'));
 
-app.use('/api', createProxyMiddleware({ target: 'http://api.novelai.net/', changeOrigin: true, pathRewrite: { '/api': '' } }));
+app.use('/api', createProxyMiddleware({
+	target: 'http://api.novelai.net/',
+	changeOrigin: true,
+	pathRewrite: { '/api': '' },
+	selfHandleResponse: true,
+	on: {
+		proxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+			res.removeHeader('content-security-policy');
+
+			return responseBuffer;
+		}),
+	  },
+}));
 
 app.get('/', function(req, res, next) {
 	res.sendFile(__dirname + '/index.html');
