c10dd4df6a5446e15f2c385e04beded73a76e288
JioChoi <jiochoi7@gmail.com>
2024-01-24 01:19:05 +0900

    Double declaration

--- index.js
+++ index.js
@@ -232,7 +232,6 @@ if(production) {
 		init();
 		//loadCSV();
 	});
-	var http = require('http');
 	http.createServer(function (req, res) {
 		res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
 		res.end();
