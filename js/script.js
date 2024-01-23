830ec6d343c52dcb0a88898d39103f43df9c8abd
JioChoi <jiochoi7@gmail.com>
2024-01-24 02:14:01 +0900

    Using resolve?

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ async function downloadLists() {
 		downloaded++;
 	});
 
-	get("/key.csv", null, "text").then((data) => {
+	get("https://huggingface.co/Jio7/NAI-Prompt-Randomizer/resolve/main/key.csv", null, "text").then((data) => {
 		keys = data.split("\n");
 		for (let i = 0; i < keys.length; i++) {
 			keys[i] = keys[i].split("|");
